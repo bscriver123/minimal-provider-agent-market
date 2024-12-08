d09bffe07e54c5d5f95508250a6a3f487d232391
bscriver123 <bsc_river123@proton.me>
2024-12-08 21:45:49 +0100

    remove create secrets as it is unnecessary

--- create_secrets.sh
+++
@@ -1,8 +0,0 @@
-source .env
-
-SECRETS_NAME=$PROJECT_NAME-$FOUNDATION_MODEL_NAME-secrets
-echo "Creating $SECRETS_NAME secrets..."
-
-SECRET_STRING="{\"OPENAI_API_KEY\":\"$OPENAI_API_KEY\",\"MARKET_API_KEY\":\"$MARKET_API_KEY\"}"
-
-aws secretsmanager create-secret --name $SECRETS_NAME --secret-string "$SECRET_STRING"
