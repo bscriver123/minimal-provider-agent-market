2e0af108400272031bb2f55a8310a981a99bd9d2
bscriver123 <bsc_river123@proton.me>
2024-12-08 22:12:39 +0100

    change aider cache

--- src/p1.py
+++ src/p1.py
@@ -9,20 +9,19 @@ ENV_VARS = {key: os.getenv(key) for key in os.environ.keys()}
 
 
 def solve_instance_with_aider(repo_directory: str):
-    client = docker_from_env()
-    # docker run -it --user $(id -u):$(id -g) --volume $(pwd)/.:/app
-    # --entrypoint /bin/bash --env-file .env paulgauthier/aider
     docker_client = docker_from_env()
     container = docker_client.containers.run(
         DOCKER_IMAGE,
         "/bin/bash",
         user=f"{os.getuid()}:{os.getgid()}",
-        volumes={f"{repo_directory}/.": {"bind": "/app", "mode": "rw"}},
+        volumes={
+            f"{repo_directory}/.": {"bind": "/app", "mode": "rw"},
+            "/tmp/aider_cache": {"bind": "/home/ubuntu", "mode": "rw"},
+        },
         environment=ENV_VARS,
         detach=True,
         tty=True,
         stdin_open=True,
-        # remove=True,
     )
 
 
