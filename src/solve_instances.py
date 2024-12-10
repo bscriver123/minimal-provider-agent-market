63e4ac0d4d2463da1358833f80ece48cc8864e93
bscriver123 <bsc_river123@proton.me>
2024-12-10 19:26:34 +0100

    improve absolute path

--- src/solve_instances.py
+++ src/solve_instances.py
@@ -61,9 +61,10 @@ def _solve_instance(instance_id: str, instance_background: str, settings: Settin
                 settings.github_username, settings.github_email, repo_absolute_path
             )
 
-            utils.copy_file_to_directory(
-                f"{os.getcwd()}/src/aider_solver/modify_repo.py", repo_absolute_path
+            modify_repo_absolute_path = (
+                Path(os.path.dirname(os.path.abspath(__file__))) / "aider_solver" / "modify_repo.py"
             )
+            utils.copy_file_to_directory(modify_repo_absolute_path, repo_absolute_path)
             utils.change_directory_ownership_recursive(repo_absolute_path, os.getuid(), os.getgid())
             aider_solver.launch_container_with_repo_mounted(
                 str(repo_absolute_path), settings.foundation_model_name.value, instance_background
